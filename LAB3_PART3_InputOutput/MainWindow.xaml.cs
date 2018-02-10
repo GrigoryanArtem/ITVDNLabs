60f4af99e77259a3083579d3b7c50c9857a03482
GrigoryanArtem <artem.thebest@mail.ru>
2018-02-11 00:32:55 +0200

    Added lab3:part3 - input/output

---
+++ LAB3_PART3_InputOutput/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace LAB3_PART3_InputOutput
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<System.Windows.Media.Color?> e)
+        {
+            ctrlColorLabel.Foreground = new SolidColorBrush(CalculateForegroundColor(e.NewValue));
+        }
+
+        private Color CalculateForegroundColor(Color? backgoundColor)
+        {
+            if (backgoundColor is null)
+                return Colors.Black;
+
+            Color color = backgoundColor.Value;
+            return (color.R + color.G + color.B) >= 350 || color.A <= 70 ? Colors.Black : Colors.White;
+        }
+    }
+}
