55a2ca3e6de26b035ad596a44e55f4de4d571ed8
GrigoryanArtem <artem.thebest@mail.ru>
2018-02-10 21:58:14 +0200

    Added lab 3:part 1 - input/output

---
+++ LAB3_PART1_InputOuput/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LAB3_PART1_InputOuput
+{
+    class Program
+    {
+        private static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            using (StreamWriter stream = new StreamWriter(File.OpenWrite("temp.txt")))
+            {
+                stream.Write(GenerateSomeText(100));
+            }
+
+            using (var reader = File.OpenText("temp.txt"))
+            {
+                Console.WriteLine(reader.ReadToEnd());
+            }
+        }
+
+        static string GenerateSomeText(int length)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                for (int i = 0; i < length; i++)
+                    sw.Write(GenerateLetter());
+
+                return sw.ToString();
+            }
+        }
+
+        static char GenerateLetter()
+        {
+            return (char)rnd.Next('a', 'z');
+        }
+    }
+}
