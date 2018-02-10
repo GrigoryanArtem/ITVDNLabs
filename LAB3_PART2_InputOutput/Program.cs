73f6afab54e0693602cb355eb47992975550f491
GrigoryanArtem <artem.thebest@mail.ru>
2018-02-10 23:00:12 +0200

    Added lab3:part2 - input/output

---
+++ LAB3_PART2_InputOutput/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace LAB3_PART2_InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = Console.ReadLine();
+
+            var rootDirectory = new DirectoryInfo(".");
+            var files = rootDirectory.GetFiles(fileName, SearchOption.TopDirectoryOnly);
+
+            if(files.Count() == 0)
+            {
+                Console.WriteLine("file not exist");
+            }
+            else if(files.Count() > 1)
+            {
+                Console.WriteLine($"finded files:");
+
+                foreach(var file in files)
+                    Console.WriteLine(file.Name);
+            }
+            else
+            {
+                var file = files.First();
+
+                Console.WriteLine(file.Name);
+
+                using (var reader = file.OpenText())
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+
+                using (var reader = file.OpenRead())
+                {
+                    using (GZipStream cmps = new GZipStream(File.Open($"{file.FullName}.zip", FileMode.OpenOrCreate,
+                        FileAccess.Write, FileShare.Read), CompressionMode.Compress))
+                    {
+                        reader.CopyTo(cmps);
+                    }
+                }
+            }
+        }
+    }
+}
